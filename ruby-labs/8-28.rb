709d3fd5ff0a08627d5be230eb762111a58be27a
Maxwell Bigman <mbigman@Maxwells-MacBook-Pro-4.local>
2017-08-30 08:04:42 -0700

    Adding some new labs

---
+++ ruby-labs/8-28.rb
@@ -0,0 +1,6 @@
+puts "Good morning" # this is a String (and this is a comment!)
+puts "Good Morning" + " Briana" # we can "add" strings together
+puts "Good morning".upcase # convert everything to capital letters
+puts "Good Morning" + " Briana".upcase # dot methods only work on the string that they are attached to
+puts "Good morning".reverse # reverse the string from backward to forward
+puts "Good MorninG".swapcase # change uppercase to lowercase and vice versa
